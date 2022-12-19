3e65d7f335b71b0f195476f08a63cb485be14746
contini-aron <contini.aron99@protonmail.com>
2022-12-19 22:26:00 +0100

    fixed double packer init

--- lua/default/packer.lua
+++ lua/default/packer.lua
@@ -21,19 +21,6 @@ end
 
 local packer_bootstrap = ensure_packer()
 
-return require('packer').startup(function(use)
-  use 'wbthomason/packer.nvim'
-  -- My plugins here
-  -- use 'foo1/bar1.nvim'
-  -- use 'foo2/bar2.nvim'
-
-  -- Automatically set up your configuration after cloning packer.nvim
-  -- Put this at the end after all plugins
-  if packer_bootstrap then
-    require('packer').sync()
-  end
-end)
-
 -- Autocommand that reloads neovim whenever you save the packer.lua file
 vim.cmd([[
   augroup packer_user_config
